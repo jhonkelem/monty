e876f04f9deb9b6e879113f740e29bf73f983728
Yohannes Kelem <99354658+jhonkelem@users.noreply.github.com>
2022-06-15 14:43:37 +0200

    Update 3-strcmp.c

--- 3-strcmp.c
+++ 3-strcmp.c
@@ -1,1 +1,26 @@
 #include "monty.h"
+
+/**
+ * _strcmp - function that will compare 2 strings
+ * @s1: count s1
+ * @s2: count for s2
+ * Return: returns the diference between s1 and s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int x;
+	int y = 0;
+	int rest;
+
+	for (x = 0; s1[x] != '\0'; x++, y++)
+	{
+		rest = s1[x] - s2[y];
+
+			if (rest != 0)
+			{
+				break;
+			}
+	}
+	return (rest);
+
+}
