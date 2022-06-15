b441c221471e33f5cff92f5a5103bfc6510c5a94
Yohannes Kelem <99354658+jhonkelem@users.noreply.github.com>
2022-06-15 14:45:36 +0200

    Update freall.c

--- freall.c
+++ freall.c
@@ -1,2 +1,20 @@
 #include "monty.h"
 #include <stdio.h>
+
+/**
+ * free_dlistint - function to free memory with doubly linked list elements
+ * @head: pointer to head of the list
+ * Return: Do not return
+ */
+void free_dlistint(stack_t *head)
+{
+	stack_t *currentno;
+
+	currentno = head;
+	while (currentno != NULL)
+	{
+		currentno = head->next;
+		free(head);
+		head = currentno;
+	}
+}
